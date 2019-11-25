7eb2b5b96ce81fb189193ac47669f51971c212ea
camilocodes <cvillami@unb.ca>
2019-11-25 15:43:50 -0400

    COMP-5 Break migrated source coordinates into X and Y

--- custom/modules/comp_migration/src/Event/SubjectMigrateEvent.php
+++ custom/modules/comp_migration/src/Event/SubjectMigrateEvent.php
@@ -12,27 +12,44 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class SubjectMigrateEvent implements EventSubscriberInterface {
 
-    use CompMigrationTrait;
+  use CompMigrationTrait;
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents() {
-      $events[MigrateEvents::PREPARE_ROW][] = ['onPrepareRow', 0];
-      return $events;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[MigrateEvents::PREPARE_ROW][] = ['onPrepareRow', 0];
+    return $events;
+  }
 
-    /**
-     * React to a new row.
-     *
-     * @param \Drupal\migrate_plus\Event\MigratePrepareRowEvent $event
-     *   The prepare-row event.
-     */
-    public function onPrepareRow(MigratePrepareRowEvent $event) {
-      $row = $event->getRow();
-      $migration = $event->getMigration();
-      $migration_id = $migration->id();
+  /**
+   * React to a new row.
+   *
+   * @param \Drupal\migrate_plus\Event\MigratePrepareRowEvent $event
+   * The prepare-row event.
+   * This is where most of the transformations from the original data to the
+   * new content types will take place. Post row migration.
+   */
+  public function onPrepareRow(MigratePrepareRowEvent $event) {
+    // Get row and migration.
+    $row = $event->getRow();
+    $migration = $event->getMigration();
+    $migration_id = $migration->id();
 
-      $row->setSourceProperty('top_x', 100);
-    }
+    // Subject photo coordinates.
+    // Top.
+    $coord_top = explode(',', $row->getSourceProperty('coord_top'));
+    $top_x = (float) $coord_top[0];
+    $top_y = (float) $coord_top[1];
+
+    $row->setSourceProperty('top_x', $top_x);
+    $row->setSourceProperty('top_y', $top_y);
+    // Bottom.
+    $coord_bottom = explode(',', $row->getSourceProperty('coord_bottom'));
+    $bottom_x = (float) $coord_bottom[0];
+    $bottom_y = (float) $coord_bottom[1];
+
+    $row->setSourceProperty('bottom_x', $bottom_x);
+    $row->setSourceProperty('bottom_y', $bottom_y);
+  }
 }
