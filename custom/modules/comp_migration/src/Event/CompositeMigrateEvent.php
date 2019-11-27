e7cab9f7e879c468d4151412b271b90d1ad09bce
camilocodes <cvillami@unb.ca>
2019-11-27 12:59:51 -0400

    COMP-5 Break appart and migrate composite contributors as terms

--- custom/modules/comp_migration/src/Event/CompositeMigrateEvent.php
+++ custom/modules/comp_migration/src/Event/CompositeMigrateEvent.php
@@ -6,6 +6,7 @@ use Drupal\comp_migration\CompMigrationTrait;
 use Drupal\migrate_plus\Event\MigrateEvents;
 use Drupal\migrate_plus\Event\MigratePrepareRowEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Defines the migrate event subscriber.
@@ -26,8 +27,46 @@ class CompositeMigrateEvent implements EventSubscriberInterface {
      * React to a new row.
      *
      * @param \Drupal\migrate_plus\Event\MigratePrepareRowEvent $event
-     *   The prepare-row event.
+     * The prepare-row event.
+     * This is where most of the transformations from the original data to the
+     * new content types will take place. Post row migration.
      */
     public function onPrepareRow(MigratePrepareRowEvent $event) {
+      // Transformations for fields not copied straight from source.
+
+      // Get row and migration.
+      $row = $event->getRow();
+      $migration = $event->getMigration();
+      $migration_id = $migration->id();
+
+      // Contributors.
+      $src_contribs = $row->getSourceProperty('contributor');
+      $contribs = explode(',', $src_contribs);
+      // Term IDs.
+      $tids = [];
+
+      foreach ($contribs as $contrib) {
+        $terms = \Drupal::entityQuery('taxonomy_term')
+          ->condition('vid', 'contributor')
+          ->condition('name', $contrib)
+          ->execute();
+
+        reset($terms);
+        $tid = key($terms);
+
+        if (empty($tid)) {
+          $term = Term::create([
+            'name' => $contrib,
+            'vid' => 'contributor',
+          ]);
+
+          $term->save();
+          $tid = $term->id();
+        }
+
+        $tids[] = $tid ? $tid : NULL;
+      }
+
+      $row->setSourceProperty('taxo_contributors', $tids);
     }
 }
