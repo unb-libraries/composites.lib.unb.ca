3577d7d7c2a6e44a085059d6d73632d63c387085
camilocodes <cvillami@unb.ca>
2020-01-08 16:11:28 -0400

    COMP-5 Add util script to delete ALL files for migration debug

---
+++ custom/modules/comp_migration/util/clrfiles.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains clrfiles.php. Deletes ALL managed FILES.
+ */
+
+use Drupal\file\Entity\File;
+
+$file_storage = \Drupal::entityTypeManager()->getStorage('file');
+$fids = Drupal::entityQuery('file')->condition('status', 1)->execute();
+
+foreach ($fids as $fid) {
+  $file = File::load($fid);
+  $file->delete();
+}
