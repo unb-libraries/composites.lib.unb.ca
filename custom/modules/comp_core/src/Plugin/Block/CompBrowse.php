fb0fbb3b0a2adb453571d9cfc2681b116b500e34
camilocodes <cvillami@unb.ca>
2020-01-30 15:00:45 -0400

    COMP-7 Add custom browse block and reconfigure/restyle

---
+++ custom/modules/comp_core/src/Plugin/Block/CompBrowse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\comp_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a custom Browse block for the home page.
+ *
+ * @Block(
+ *   id = "comp_browse",
+ *   admin_label = @Translation("Browse"),
+ *   category = @Translation("Misc"),
+ * )
+ */
+class CompBrowse extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $text = '
+      <p>
+        <br>
+        <a href="/browse-composites">By Year</a>
+        <br>
+        <a href="/browse-subjects">All Subjects</a>
+      </p>
+    ';
+
+    return [
+      '#markup' => $this->t($text),
+    ];
+  }
+
+}
