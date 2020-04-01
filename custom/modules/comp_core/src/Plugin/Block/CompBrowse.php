a8fb8d9a9484983faac8ddd342ee3c5b80137f4c
camilocodes <camilocodes@gmail.com>
2020-04-01 14:53:58 -0300

    COMP-30 Update labels

--- custom/modules/comp_core/src/Plugin/Block/CompBrowse.php
+++ custom/modules/comp_core/src/Plugin/Block/CompBrowse.php
@@ -23,7 +23,7 @@ class CompBrowse extends BlockBase {
       <p>
         <a href="/browse-composites">By Year</a>
         <br>
-        <a href="/browse-subjects">All People</a>
+        <a href="/browse-by-name">By Name</a>
       </p>
     ';
 
