b9cfa27948a672c241fd4ade1fc226943b650d37
camilocodes <camilocodes@gmail.com>
2020-04-22 13:45:15 -0300

    COMP-40 Restrict routes and hide links to restricted use admin

--- custom/modules/comp_core/src/EventSubscriber/RouteSubscriber.php
+++ custom/modules/comp_core/src/EventSubscriber/RouteSubscriber.php
@@ -14,8 +14,20 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   public function alterRoutes(RouteCollection $collection) {
-    if ($route = $collection->get('user.pass')) {
-      if (!\Drupal::currentUser()->isAuthenticated()) {
+    $halt_routes = [
+      'user.page',
+      'user.pass',
+      'user.register',
+      'user.reset.form',
+      'entity.user.edit_form',
+    ];
+
+    foreach ($halt_routes as $halt_route) {
+      $route = $collection->get($halt_route);
+      $usr = \Drupal::currentUser();
+
+      // Deny route access if user is visitor or content editor.
+      if (!$usr->isAuthenticated() or $usr->hasRole('content_editor')) {
         $route->setRequirement('_access', 'FALSE');
       }
     }
