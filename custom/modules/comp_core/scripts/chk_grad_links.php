8a70bb9b78f467635c2f7aa6e1d301c4c40179b6
cvillami <cvillami@unb.ca>
2021-03-17 12:04:00 -0300

    COMP-33 Add utility script to list outdated graduation ceremony links

---
+++ custom/modules/comp_core/scripts/chk_grad_links.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains chk_grad_links.php.
+ *
+ * Checks all composite for reachable graduation ceremony links, and reports
+ * fails.
+ */
+
+use Drupal\node\Entity\Node;
+
+// Search for all composite node ids.
+$cids = Drupal::entityQuery('node')
+  ->condition('type', 'composite')->execute();
+
+// Load and process all composites.
+foreach ($cids as $cid) {
+  $composite = Node::load($cid);
+  $grad_url = $composite->field_grad_ceremony->getValue()[0]['uri'];
+  $title = $composite->getTitle();
+
+  if ($grad_url and !url_valid($grad_url)) {
+    echo "\n$title";
+  }
+}
+
+echo "\n";
