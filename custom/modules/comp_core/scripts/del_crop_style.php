e9c940789cf166f7791b18ab0fcd9f4242a3d3c0
camilocodes <cvillami@unb.ca>
2021-12-15 14:22:02 -0400

    COMP-116 Add script to delete all subject crop image styles

---
+++ custom/modules/comp_core/scripts/del_crop_style.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains del_crop_styles.php.
+ *
+ * Deletes ALL subject crop image styles.
+ */
+
+use Drupal\image\Entity\ImageStyle;
+
+// Load all styles.
+$styles = \Drupal::entityQuery('image_style')
+  ->execute();
+
+// Run through styles.
+foreach ($styles as $style_name) {
+  // If the style correponds to a composite crop...
+  if (strpos($style_name, 'composite_crop') !== FALSE) {
+    // Load, delete, and flush style.
+    $style = ImageStyle::load($style_name);
+    $style->delete();
+    $style->flush();
+  }
+}
