aad87a96677b0972c11272f43053b54072cbbfcd
camilocodes <cvillami@unb.ca>
2021-12-15 16:15:55 -0400

    COMP-116 Mass delete image styles from configuration

---
+++ custom/modules/comp_core/scripts/del_all_styles.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains del_all_styles.php.
+ *
+ * Deletes ALL image styles.
+ */
+
+// Load all styles.
+$result = \Drupal::entityQuery("image_style")
+  ->execute();
+
+$storage_handler = \Drupal::entityTypeManager()->getStorage("image_style");
+$entities = $storage_handler->loadMultiple($result);
+$storage_handler->delete($entities);
