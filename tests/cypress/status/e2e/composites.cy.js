80eb4c7de11ce79f39e843eb0748bd33a3886af7
patschilf <46682967+patschilf@users.noreply.github.com>
2022-08-16 10:23:35 -0300

    TESTING-1 Migrate composites

---
+++ tests/cypress/status/e2e/composites.cy.js
@@ -0,0 +1,21 @@
+const host = 'https://composites.lib.unb.ca'
+describe('UNB Class Composite & Group Photographs', {baseUrl: host}, () => {
+
+  context('Front page', {baseUrl: host}, () => {
+    beforeEach(() => {
+      cy.visit('')
+      cy.title()
+        .should('contain', 'UNB Class Composite & Group Photographs')
+    })
+
+    specify('Search for "Max Aitken" should find 5+ results', () => {
+      cy.get('form').within(() => {
+        cy.get('input[type="text"]').type('Max Aitken')
+      }).submit()
+      cy.url()
+        .should('match', /search-persons/)
+      cy.get('.item-list ul li')
+        .should('have.lengthOf.at.least', 5)
+    })
+  })
+})
