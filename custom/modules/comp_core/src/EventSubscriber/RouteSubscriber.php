fa610d00627e88f4f5e794a4422f7a575d281c5c
Camilo Villamizar <cvillami@unb.ca>
2023-01-24 15:51:04 -0400

    COMP-131 Dependency inject @current_user in comp_core RouteSubscriber

--- custom/modules/comp_core/src/EventSubscriber/RouteSubscriber.php
+++ custom/modules/comp_core/src/EventSubscriber/RouteSubscriber.php
@@ -3,12 +3,30 @@
 namespace Drupal\comp_core\EventSubscriber;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\User\Entity\User;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Listens to the dynamic route events.
  */
 class RouteSubscriber extends RouteSubscriberBase {
+  /**
+   * Current user session.
+   *
+   * @var Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUserSession;
+
+  /**
+   * Constructs a new RouteSubscriber object.
+   *
+   * @param Drupal\Core\Session\AccountProxy $currentUserSession
+   *   The current user session.
+   */
+  public function __construct(AccountProxy $currentUserSession) {
+    $this->currentUserSession = $currentUserSession;
+  }
 
   /**
    * {@inheritdoc}
@@ -23,7 +41,7 @@ class RouteSubscriber extends RouteSubscriberBase {
 
     foreach ($halt_routes as $halt_route) {
       $route = $collection->get($halt_route);
-      $usr = \Drupal::currentUser();
+      $usr = User::load($this->currentUserSession->id());
 
       // Deny route access if user is visitor or content editor.
       if (!$usr->isAuthenticated() or in_array('content_editor', $usr->getRoles())) {
