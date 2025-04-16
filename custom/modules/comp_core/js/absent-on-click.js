73f5ff7cbe0e04041ada02b59f1860d407009ae9
Camilo Villamizar <cvillami@unb.ca>
2025-04-16 15:02:52 -0300

    COMP-148 Rewrite absent-on-click.js without jQuery and using once()

--- custom/modules/comp_core/js/absent-on-click.js
+++ custom/modules/comp_core/js/absent-on-click.js
@@ -1,31 +1,36 @@
-(function($) {
+(function (Drupal, once) {
   'use strict';
 
   // Set empty coordinate controls to zero if 'absent' is checked.
   Drupal.behaviors.absentOnClick = {
     attach: function (context, settings) {
-      // On document ready.
-      $(document).ready( function() {
-        // When 'absent' checkbox is clicked.
-        $('#edit-field-absent-value').click( function() {
+      // Select the 'absent' checkbox and attach the behavior only once.
+      const absentCheckbox = once('absent-on-click', '#edit-field-absent-value', context);
+      absentCheckbox.forEach((checkbox) => {
+        checkbox.addEventListener('click', function () {
           // If 'absent' has been checked.
-          if ($(this).prop('checked')) {
+          if (checkbox.checked) {
             // Zero all empty coordinates.
-            if (!$('#edit-field-top-x-0-value').val()) {
-              $('#edit-field-top-x-0-value').val('0');
+            const topX = document.querySelector('#edit-field-top-x-0-value');
+            const topY = document.querySelector('#edit-field-top-y-0-value');
+            const bottomX = document.querySelector('#edit-field-bottom-x-0-value');
+            const bottomY = document.querySelector('#edit-field-bottom-y-0-value');
+
+            if (topX && !topX.value) {
+              topX.value = '0';
             }
-            if (!$('#edit-field-top-y-0-value').val()) {
-              $('#edit-field-top-y-0-value').val('0');
+            if (topY && !topY.value) {
+              topY.value = '0';
             }
-            if (!$('#edit-field-bottom-x-0-value').val()) {
-              $('#edit-field-bottom-x-0-value').val('0');
+            if (bottomX && !bottomX.value) {
+              bottomX.value = '0';
             }
-            if (!$('#edit-field-bottom-y-0-value').val()) {
-              $('#edit-field-bottom-y-0-value').val('0');
+            if (bottomY && !bottomY.value) {
+              bottomY.value = '0';
             }
           }
         });
       });
     },
   };
-})(jQuery);
+})(Drupal, once);
