9b6eff3b16f6027a35e94329c0110c02be91fd94
Camilo Villamizar <cvillami@unb.ca>
2025-10-07 14:54:28 -0300

    COMP-151 Capture cropper coordinates on blur

--- custom/modules/comp_cms_extend/js/cropper_init.js
+++ custom/modules/comp_cms_extend/js/cropper_init.js
@@ -1,14 +1,21 @@
 (function (Drupal, once) {
   Drupal.behaviors.compCmsCropper = {
     attach: function (context, settings) {
-      // Find the image(s)
       once('compCmsCropper', '#subject-img', context).forEach(function(img) {
-        // Confirm the element exists
         console.log('Initializing Cropper for:', img);
 
-        // Initialize CropperJS
         var cropper = new Cropper(img, {
           viewMode: 1,
+          // Freeform cropping (no aspect ratio)
+          aspectRatio: NaN,
+          // Listen for cropbox release
+          cropend: function () {
+            // Get cropping data
+            var data = cropper.getData();
+            // Coordinates: data.x, data.y, data.width, data.height
+            console.log('Crop coordinates:', data);
+            // You can now store these coordinates in hidden fields, or send them via AJAX, etc.
+          }
         });
       });
     }
